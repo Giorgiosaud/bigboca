31d8001107bda8bd144f1a9921ae126c17495910
Giorgiosaud <jorgelsaud@gmail.com>
2015-06-28 22:35:19 -0430

    test new way to show ps

--- js/main.js
+++ js/main.js
@@ -105,7 +105,7 @@ jQuery(document).ready(function ($) {
     firstAnimation.insert(TweenMax.fromTo($('.scrollDown'), 4, {opacity: 1}, {opacity: 0}))
     firstAnimation.insert(TweenMax.fromTo($('.logoContainer'), 4, {opacity: 1}, {opacity: 0}))
     firstAnimation.insert(TweenMax.from($('.mensajeInicial:eq(0)'), 1, {marginTop: "-8em"}));
-    firstAnimation.insert(TweenMax.from($('.se-slope:first-child .se-content'), 1, {y: "-14%"}));
+    firstAnimation.insert(TweenMax.from($('.se-slope:first-child .se-content:eq(0)'), 1, {y: "-14%"}));
     firstAnimation.insert(TweenMax.from($('.menuPrincipal'), 1, {marginLeft: -2 * elementosComunes.anchoMenuCerrado}));
     var scene = new ScrollMagic.Scene({
         triggerElement: "#trigger1",
