4991c6821afd9d1cfdfcdaa268c0a1b5eeb69eae
Giorgiosaud <jorgelsaud@gmail.com>
2015-07-16 15:56:55 -0430

    fix

--- inc/customShortcode/shortCodes/capabilitiesSection.php
+++ inc/customShortcode/shortCodes/capabilitiesSection.php
@@ -6,21 +6,22 @@ function capabilitiesSection_shortcode($atts)
 
     // Attributes
     extract(shortcode_atts(
-            array(
-                'comment'  => '"No other sampling program in the USA can expose your brand to this many Hispanic consumers under one roof, I guarantee this!”',
-                'author'   => 'Rhandy Taveras',
-                'position' => 'President, Big Boca Group.',
-                'fondo'    => 'http://bigboca.test/wp-content/uploads/2015/06/city.jpg',
-                'id'       => 'Capabilities'
+        array(
+            'comment'  => '"No other sampling program in the USA can expose your brand to this many Hispanic consumers under one roof, I guarantee this!”',
+            'author'   => 'Rhandy Taveras',
+            'position' => 'President, Big Boca Group.',
+            'fondo'    => 'http://bigboca.test/wp-content/uploads/2015/06/city.jpg',
+            'id'       => 'Capabilities'
             ), $atts)
     );
     // Code
     ob_start();?>
-    <section id="CapabilitiesSection" class="se-container">
-        <div id="<?= $id ?>" class="se-slope">
-            <article class="se-content"
-                     style='background: -webkit-linear-gradient(rgba(0, 148, 255, 0.45), rgba(0, 81, 255, 0.45)), url("<?= $fondo ?>") !important;
-                         background: linear-gradient( rgba(0, 148, 255, 0.45), rgba(0, 81, 255, 0.45) ), url("<?= $fondo ?>") !important;'
+    <div class="capabilitiesContainer">
+        <section id="CapabilitiesSection" class="se-container">
+            <!-- <div id="<?= $id ?>" class="se-slope">
+                <article class="se-content"
+                style='background: -webkit-linear-gradient(rgba(0, 148, 255, 0.45), rgba(0, 81, 255, 0.45)), url("<?= $fondo ?>") !important;
+                background: linear-gradient( rgba(0, 148, 255, 0.45), rgba(0, 81, 255, 0.45) ), url("<?= $fondo ?>") !important;'
                 >
                 <div class="col-xs-11 text-center mensajeInicial">
                     <?= $comment ?>
@@ -33,7 +34,7 @@ function capabilitiesSection_shortcode($atts)
                 </div>
                 <div class="clearfix"></div>
             </article>
-        </div>
+        </div> -->
         <?php
         $query = new WP_Query(array(
             'post_type'   => 'capabilities',
@@ -41,148 +42,149 @@ function capabilitiesSection_shortcode($atts)
             'meta_key'    => 'orden',
             'orderby'     => 'meta_value_num',
             'order'       => 'ASC'
-        ));
+            ));
         $CapabilitiesDefinitionsIndex = 0;
         if ($query->have_posts()) {
             while ($query->have_posts()) : $query->the_post();
-                $CapabilitiesDefinitionsIndex ++;
+            $CapabilitiesDefinitionsIndex ++;
+            $id = get_field('id');
+            $urlBg = get_field('imagendefondo');
+            $color_fondo1 = get_field('color_fondo_1');
+            $opacity1 = get_field('opacity_1');
+            $color_fondo2 = get_field('color_fondo_2');
+            $opacity2 = get_field('opacity_2');
+            $style = 'background: -webkit-linear-gradient(' . hex2rgba($color_fondo1,
+                $opacity1) . ', ' . hex2rgba($color_fondo2, $opacity2) . '), url("' . $urlBg['url'] . '") !important;
+background: linear-gradient( ' . hex2rgba($color_fondo1,
+    $opacity1) . ', ' . hex2rgba($color_fondo2,
+    $opacity2) . ' ), url("' . $urlBg['url'] . '") !important;  background-size: cover !important;';
+if ($CapabilitiesDefinitionsIndex % 2 != 0) {
+    $iconoClass = 'iconoRight';
+    $descriptionClass = '';
+    $descriptionAlignment = 'center';
+
+} else {
+    $iconoClass = 'iconoLeft';
+    $descriptionClass = 'descripcionLeft';
+    $descriptionAlignment = 'center';
+}
+
+if (has_children()) {
+    $id2 = get_the_ID();
+    $id = get_field('id');
+    $title = get_the_title();
+    $content = get_the_content();
+    $urlBg = get_field('imagendefondo');
+    $color_fondo1 = get_field('color_fondo_1');
+    $color_fondo2 = get_field('color_fondo_2');
+    $style = 'background: -webkit-linear-gradient(' . $color_fondo1 . ', ' . $color_fondo2 . '), url("' . $urlBg['url'] . '") !important;
+    background: linear-gradient( ' . $color_fondo1 . ', ' . $color_fondo2 . ' ), url("' . $urlBg['url'] . '") !important';
+    $colorIndicadores = get_field('color_de_indicadores');
+    $colorBordeIndicadores = get_field('color_de_borde_indicadores');
+    $colorDeFlechas = get_field('color_de_flechas');
+    $cantidadDeHijos = 0;
+    $childrenArgs = array(
+        'post_parent' => $id2,
+        'post_type'   => 'capabilities',
+        'post_status' => 'publish',
+        'orderby'=>'menu_order',
+        'order'=>'ASC'
+        );
+    $childrens = new WP_Query($childrenArgs);
+
+
+
+    ?>
+
+    <div id="<?= $id ?>" class="se-slope" style='<?= $style ?>'>
+        <article class="se-content">
+            <div
+            class="<?= $descriptionClass ?> col-xs-12 col-sm-10 col-md-8 col-sm-push-1 col-md-push-2">
+            <?= showCapability(get_field('icono'), $title, $content, $descriptionAlignment) ?>
+            <div class="showChildrensButton showChildrensButton2">
+                Learn More +
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="hidden-slope"></div>
+        <div class="hiddenChildrens blueBocaText">
+            <?php
+            if ($childrens->have_posts()) {
+                $CapabilitiesDefinitionsIndexChild == 0;
+                while ($childrens->have_posts()) : $childrens->the_post();
                 $id = get_field('id');
+                $CapabilitiesDefinitionsIndexChild ++;
                 $urlBg = get_field('imagendefondo');
                 $color_fondo1 = get_field('color_fondo_1');
                 $opacity1 = get_field('opacity_1');
                 $color_fondo2 = get_field('color_fondo_2');
                 $opacity2 = get_field('opacity_2');
                 $style = 'background: -webkit-linear-gradient(' . hex2rgba($color_fondo1,
-                        $opacity1) . ', ' . hex2rgba($color_fondo2, $opacity2) . '), url("' . $urlBg['url'] . '") !important;
-                             background: linear-gradient( ' . hex2rgba($color_fondo1,
-                        $opacity1) . ', ' . hex2rgba($color_fondo2,
-                        $opacity2) . ' ), url("' . $urlBg['url'] . '") !important;  background-size: cover !important;';
-                if ($CapabilitiesDefinitionsIndex % 2 != 0) {
-                    $iconoClass = 'iconoRight';
-                    $descriptionClass = '';
-                    $descriptionAlignment = 'center';
+                    $opacity1) . ', ' . hex2rgba($color_fondo2,
+                    $opacity2) . '), url("' . $urlBg['url'] . '") !important;
+background: linear-gradient( ' . hex2rgba($color_fondo1,
+    $opacity1) . ', ' . hex2rgba($color_fondo2,
+    $opacity2) . ' ), url("' . $urlBg['url'] . '") !important;  background-size: cover !important;';
+if ($CapabilitiesDefinitionsIndexChild % 2 == 0) {
+    $iconoClass = 'iconoRight';
+    $descriptionClass = '';
+    $descriptionAlignment = 'center';
 
-                } else {
-                    $iconoClass = 'iconoLeft';
-                    $descriptionClass = 'descripcionLeft';
-                    $descriptionAlignment = 'center';
-                }
+} else {
+    $iconoClass = 'iconoLeft';
+    $descriptionClass = 'descripcionLeft';
+    $descriptionAlignment = 'center';
+}
 
-                if (has_children()) {
-                    $id2 = get_the_ID();
-                    $id = get_field('id');
-                    $title = get_the_title();
-                    $content = get_the_content();
-                    $urlBg = get_field('imagendefondo');
-                    $color_fondo1 = get_field('color_fondo_1');
-                    $color_fondo2 = get_field('color_fondo_2');
-                    $style = 'background: -webkit-linear-gradient(' . $color_fondo1 . ', ' . $color_fondo2 . '), url("' . $urlBg['url'] . '") !important;
-                             background: linear-gradient( ' . $color_fondo1 . ', ' . $color_fondo2 . ' ), url("' . $urlBg['url'] . '") !important';
-                    $colorIndicadores = get_field('color_de_indicadores');
-                    $colorBordeIndicadores = get_field('color_de_borde_indicadores');
-                    $colorDeFlechas = get_field('color_de_flechas');
-                    $cantidadDeHijos = 0;
-                    $childrenArgs = array(
-                        'post_parent' => $id2,
-                        'post_type'   => 'capabilities',
-                        'post_status' => 'publish',
-                        'orderby'=>'menu_order',
-                        'order'=>'ASC'
-                    );
-                    $childrens = new WP_Query($childrenArgs);
+?>
+<!--                            <div id="--><?//= $id ?><!--" class="se-slope" style='--><?//= $style ?><!--'>-->
+<!--                                <article class="se-content">-->
+<div
+class="<?= $descriptionClass ?> blueBocaText col-xs-12 col-sm-10 col-md-8 col-sm-push-1 col-md-push-2">
+<?= showCapability(get_field('icono'), get_the_title(), get_the_content(),
+$descriptionAlignment) ?>
+<!--                                    </div>-->
+<div class="clearfix"></div>
+<!--                                </article>-->
+</div>
+<div class="separador"></div>
+<?php
+endwhile;
+}
+?>
+<div class="clearfix"></div>
+</article>
 
+</div>
+<?php
+} else {
+    ?>
 
+    <div id="<?= $id ?>" class="se-slope" style='<?= $style ?>'>
+        <article class="se-content">
+            <div
+            class="<?= $descriptionClass ?> col-xs-12 col-sm-10 col-md-8 col-sm-push-1 col-md-push-2">
+            <?= showCapability(get_field('icono'), get_the_title(), get_the_content(),
+            $descriptionAlignment) ?>
+        </div>
+        <div class="clearfix"></div>
+    </article>
+</div>
 
-                    ?>
+<?php
+}
+endwhile;
+}
 
-                    <div id="<?= $id ?>" class="se-slope" style='<?= $style ?>'>
-                        <article class="se-content">
-                            <div
-                                class="<?= $descriptionClass ?> col-xs-12 col-sm-10 col-md-8 col-sm-push-1 col-md-push-2">
-                                <?= showCapability(get_field('icono'), $title, $content, $descriptionAlignment) ?>
-                                <div class="showChildrensButton showChildrensButton2">
-                                    Learn More +
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="hidden-slope"></div>
-                            <div class="hiddenChildrens blueBocaText">
-                                <?php
-                                if ($childrens->have_posts()) {
-                                    $CapabilitiesDefinitionsIndexChild == 0;
-                                    while ($childrens->have_posts()) : $childrens->the_post();
-                                        $id = get_field('id');
-                                        $CapabilitiesDefinitionsIndexChild ++;
-                                        $urlBg = get_field('imagendefondo');
-                                        $color_fondo1 = get_field('color_fondo_1');
-                                        $opacity1 = get_field('opacity_1');
-                                        $color_fondo2 = get_field('color_fondo_2');
-                                        $opacity2 = get_field('opacity_2');
-                                        $style = 'background: -webkit-linear-gradient(' . hex2rgba($color_fondo1,
-                                                $opacity1) . ', ' . hex2rgba($color_fondo2,
-                                                $opacity2) . '), url("' . $urlBg['url'] . '") !important;
-                             background: linear-gradient( ' . hex2rgba($color_fondo1,
-                                                $opacity1) . ', ' . hex2rgba($color_fondo2,
-                                                $opacity2) . ' ), url("' . $urlBg['url'] . '") !important;  background-size: cover !important;';
-                                        if ($CapabilitiesDefinitionsIndexChild % 2 == 0) {
-                                            $iconoClass = 'iconoRight';
-                                            $descriptionClass = '';
-                                            $descriptionAlignment = 'center';
+?>
 
-                                        } else {
-                                            $iconoClass = 'iconoLeft';
-                                            $descriptionClass = 'descripcionLeft';
-                                            $descriptionAlignment = 'center';
-                                        }
+</section>
+</div>
+<?php
 
-                                        ?>
-                                        <!--                            <div id="--><?//= $id ?><!--" class="se-slope" style='--><?//= $style ?><!--'>-->
-                                        <!--                                <article class="se-content">-->
-                                        <div
-                                            class="<?= $descriptionClass ?> blueBocaText col-xs-12 col-sm-10 col-md-8 col-sm-push-1 col-md-push-2">
-                                            <?= showCapability(get_field('icono'), get_the_title(), get_the_content(),
-                                                $descriptionAlignment) ?>
-                                            <!--                                    </div>-->
-                                            <div class="clearfix"></div>
-                                            <!--                                </article>-->
-                                        </div>
-                                        <div class="separador"></div>
-                                    <?php
-                                    endwhile;
-                                }
-                                ?>
-                                <div class="clearfix"></div>
-                        </article>
+$myvariable = ob_get_clean();
 
-                    </div>
-                <?php
-                } else {
-                    ?>
-
-                    <div id="<?= $id ?>" class="se-slope" style='<?= $style ?>'>
-                        <article class="se-content">
-                            <div
-                                class="<?= $descriptionClass ?> col-xs-12 col-sm-10 col-md-8 col-sm-push-1 col-md-push-2">
-                                <?= showCapability(get_field('icono'), get_the_title(), get_the_content(),
-                                    $descriptionAlignment) ?>
-                            </div>
-                            <div class="clearfix"></div>
-                        </article>
-                    </div>
-
-                <?php
-                }
-            endwhile;
-        }
-
-        ?>
-
-    </section>
-    <?php
-
-    $myvariable = ob_get_clean();
-
-    return $myvariable;
+return $myvariable;
 }
 
 add_shortcode('capabilitiesSection', 'capabilitiesSection_shortcode');
