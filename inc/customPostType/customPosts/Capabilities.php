7e87c73429d35e29546641ae461317f78d0db3a5
Giorgiosaud <jorgelsaud@gmail.com>
2015-07-16 16:22:46 -0430

    fix

--- inc/customPostType/customPosts/Capabilities.php
+++ inc/customPostType/customPosts/Capabilities.php
@@ -37,7 +37,7 @@ function capabilities_post_type() {
         'has_archive'         => true,
         'exclude_from_search' => false,
         'publicly_queryable'  => true,
-        // 'capability_type'     => 'page',
+        'capability_type'     => 'post',
     );
     register_post_type( 'capabilities', $args );
 
