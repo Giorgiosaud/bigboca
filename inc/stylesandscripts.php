51d56074d779e127236c38b8ade531080087bf57
Giorgiosaud <jorgelsaud@gmail.com>
2015-07-23 10:00:09 -0430

    optimizing

--- inc/stylesandscripts.php
+++ inc/stylesandscripts.php
@@ -3,7 +3,7 @@ function bigbocascripts()
 {
     wp_deregister_script('jquery');
     wp_enqueue_style('allStyles', get_template_directory_uri() . '/public/css/style.css', array(), '1.0');
-    wp_enqueue_style('allJs', get_template_directory_uri() . '/public/js/all.js', array(), '1.0');
+    wp_enqueue_script('allJs', get_template_directory_uri() . '/public/js/all.js', array(), '1.0');
 }
 add_action( 'wp_enqueue_scripts', 'bigbocascripts' );
 ?>
