ec17af3417c9ec82068076449163675736b63edc
Giorgiosaud <jorgelsaud@gmail.com>
2015-07-23 11:56:21 -0430

    fix

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,13 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     $baseDir . '/inc/stylesandscripts.php',
-    $baseDir . '/inc/header_customization.php',
-    $baseDir . '/inc/customPostType/customPosts/Capabilities.php',
-    $baseDir . '/inc/customPostType/customPosts/usDefinition.php',
-    $baseDir . '/inc/customShortcode/shortCodes/contact.php',
-    $baseDir . '/inc/customShortcode/shortCodes/capabilitiesSection.php',
-    $baseDir . '/inc/customShortcode/shortCodes/contact.php',
-    $baseDir . '/inc/customShortcode/shortCodes/intro.php',
-    $baseDir . '/inc/customShortcode/shortCodes/usSection.php',
-    $baseDir . '/inc/ajax/SendEmail.php',
 );
